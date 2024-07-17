e43dd46d4dfeb86dc9d44b12c0e10a808ca47800
poseident <benjamin.schweitzer1701@gmail.com>
2024-07-17 10:47:18 +0200

    ok

--- starting0/ex08/Loading.py
+++ starting0/ex08/Loading.py
@@ -3,11 +3,31 @@ import shutil
 
 
 def format_time(seconds):
+    """
+    Format the given time in seconds as MM:SS.
+    Args:
+        seconds (float): Time in seconds.
+    Returns:
+        str: Formatted time in the format MM:SS.
+    """
     m, s = divmod(seconds, 60)
     return f"{int(m):02d}:{int(s):02d}"
 
 
 def ft_tqdm(lst: range) -> None:
+    """
+    Simulate a progress bar for iterating through a range.
+    Args:
+        lst (range): The range to iterate through.
+    Yields:
+        Any: The current item from the range.
+        is a keyword in Python used in the context of creating generators.
+        Generators are a way to create iterators, which are objects used to
+        iterate over a sequence of values without having to store all those
+        values in memory at once. Instead of generating allvalues and returning
+        them in one go, a generator yields one value at a time whenever the
+        yield statement is encountered.
+    """
     total = len(lst)
     start_time = time.time()
 
